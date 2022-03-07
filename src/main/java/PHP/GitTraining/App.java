f790260dbafadda4b4100f333842895192c17257
Omkar Ramkumar(UST <190038@ust-global.com>
2022-03-07 10:14:55 +0530

    first commit

---
+++ src/main/java/PHP/GitTraining/App.java
@@ -0,0 +1,13 @@
+package PHP.GitTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Git World" );
+    }
+}
---
+++ src/test/java/PHP/GitTraining/GitTraining.java
@@ -0,0 +1,9 @@
+package PHP.GitTraining;
+
+public class GitTraining {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to git training haha");
+	}
+}
