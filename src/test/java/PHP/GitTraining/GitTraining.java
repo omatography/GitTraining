04b4840033b431fc015dbce5ea2b7d0568b11b32
Omkar Ramkumar(UST <190038@ust-global.com>
2022-03-07 10:28:18 +0530

    adding 2 numbers

--- src/test/java/PHP/GitTraining/GitTraining.java
+++ src/test/java/PHP/GitTraining/GitTraining.java
@@ -6,5 +6,8 @@ public class GitTraining {
 	public static void main(String[] args) {
 		System.out.println("Welcome to git training haha");
 		System.out.println("my commit");
+		
+		int a=10, b=20;
+		System.out.println(a+b);
 	}
 }
