3f2b178b1663f8ddbb3759008d87ffa02f50d979
Omkar Ramkumar(UST <190038@ust-global.com>
2022-03-10 09:43:09 +0530

    first commit

--- src/main/java/PHP/GitTraining/App.java
+++ src/main/java/PHP/GitTraining/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Git World" );
+        
+        System.out.println( "Omkar" );
+        System.out.println( "Omkar" );
     }
 }
